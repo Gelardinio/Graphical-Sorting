ca6bd4faa94d80d789794142281df7e9eb9307e1
Gerald <29520085+Gelardinio@users.noreply.github.com>
2020-01-22 20:58:47 -0500

    edit

--- GraphicalSorting/allegro.h
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <allegro5/allegro.h>
-
-int initialize_allegro();
--- GraphicalSorting/mainFunctions.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "sorting.h"
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_primitives.h>
-
-void displaySort(ALLEGRO_FONT *add_font, vector<int> step, int pointerPos);
-
